0c0c3e64453590d99c179dfaf14fbdbfc64a41db
Zachary Keck <90582557+zkeck@users.noreply.github.com>
2021-09-27 23:39:33 +0000

    Simplified and cleaned up the code. Made sure it is counting correctly now.

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 from urllib.request import urlretrieve
 
+
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
 
+
+#Counts total requests in the last 6 months
 Six_month_count = 0
-total_count = 0
+
 for line in open(local_file):
-  
-  total_count += 1
-  #get the month
-  if line[0] == 'r':
-    if line[19:23] == '1995':
-      if line[15:18] == 'Oct' or line[15:18] == "Sep" or line[15:18] == "Aug" or line[15:18] == "Jul" or line[15:18] == "Jun" or line[15:18] == "May":
-        print(line[12:23])
-        Six_month_count += 1
-      #elif line[15:18] == "May":
-       # if int(line[12:14]) >= 11: 
-        #  print(line[12:23])
-         # Six_month_count += 1
- 
-  elif line[0] == 'l':
-    if line[19:23] == '1995':
-      if line[15:18] == 'Oct' or line[15:18] == "Sep" or line[15:18] == "Aug" or line[15:18] == "Jul" or line[15:18] == "Jun" or line[15:18] == "May":
-        print(line[12:23])
-        Six_month_count += 1
-      #elif line[15:18] == "May":
-        #if int(line[12:14]) >= 11: 
-          #print(line[12:23])
-          #Six_month_count += 1
-        
+  if "May/1995" in line:
+    Six_month_count += 1
+  elif "Jun/1995" in line:
+    Six_month_count += 1
+  elif "Jul/1995" in line:
+    Six_month_count += 1
+  elif "Aug/1995" in line:
+    Six_month_count += 1
+  elif "Sep/1995" in line:
+    Six_month_count += 1
+  elif "Oct/1995" in line:
+    Six_month_count += 1
  
 
-print(total_count)
-print(Six_month_count)
+#Counts total requests during the whole time period.
+total_count = 0
+
+for line in open(local_file):
+  if "1994" in line or "1995" in line:
+    total_count += 1 
+
+
+print("Total requests made in the last 6 months:",Six_month_count)
+print("Total requests made in the time period represented by the log:",total_count)
+
 
 
