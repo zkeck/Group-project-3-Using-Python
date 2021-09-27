bf08edf23f85636889c9128b8c4f1035905d6859
Zachary Keck <90582557+zkeck@users.noreply.github.com>
2021-09-27 22:33:58 +0000

    Now counts for the total and last 6 months.

--- main.py
+++ main.py
@@ -6,5 +6,35 @@ LOCAL_FILE = 'local_copy.log'
 # Use urlretrieve() to fetch a remote copy and save into the local file path
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
 
+Six_month_count = 0
+total_count = 0
 for line in open(local_file):
-  print(line)
+  
+  total_count += 1
+  #get the month
+  if line[0] == 'r':
+    if line[19:23] == '1995':
+      if line[15:18] == 'Oct' or line[15:18] == "Sep" or line[15:18] == "Aug" or line[15:18] == "Jul" or line[15:18] == "Jun" or line[15:18] == "May":
+        print(line[12:23])
+        Six_month_count += 1
+      #elif line[15:18] == "May":
+       # if int(line[12:14]) >= 11: 
+        #  print(line[12:23])
+         # Six_month_count += 1
+ 
+  elif line[0] == 'l':
+    if line[19:23] == '1995':
+      if line[15:18] == 'Oct' or line[15:18] == "Sep" or line[15:18] == "Aug" or line[15:18] == "Jul" or line[15:18] == "Jun" or line[15:18] == "May":
+        print(line[12:23])
+        Six_month_count += 1
+      #elif line[15:18] == "May":
+        #if int(line[12:14]) >= 11: 
+          #print(line[12:23])
+          #Six_month_count += 1
+        
+ 
+
+print(total_count)
+print(Six_month_count)
+
+
